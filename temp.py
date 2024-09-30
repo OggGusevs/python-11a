98e165854fe7989726dcdbb03713b6bdd3ed7474
OggGusevs <olegsgusevs14@gmail.com>
2024-09-30 13:16:03 +0300

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,29 @@
+print('hello world')
+
+a = 10 # a
+
+if a > 10 :
+
+pass
+
+a = 10
+
+b = 5
+
+print (a + b)
+
+print (a - b)
+
+print (a / b)
+
+print (a * b)
+
+print (a ** b)
+
+print (a // b)
+
+user = input('enter your name:')
+
+print(f"hello{user}!")
+
+user2 = input('enter number:')
