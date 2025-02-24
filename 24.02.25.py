c366f16f75201bb7a68b3e41dead51ddc7e85df0
OggGusevs <olegsgusevs14@gmail.com>
2025-02-24 13:19:49 +0200

    Add files via upload

---
+++ 24.02.25.py
@@ -0,0 +1,31 @@
+#1. uzd.
+
+def Sveika(name):
+    print(f'Sveika,{name}!')
+    
+Sveika("Perdel")
+
+    #2. uzd.
+    
+def sum_numbers(a,b):
+    result= a+b
+    return result
+
+print(sum_numbers(5, 3))
+
+#3. uzd.
+def is_even(n):
+    return n % 2 ==0
+
+result_4 = is_even(4)
+result_7 = is_even(7)
+
+print("4", result_4)
+print("7", result_7)
+
+#4. uzd.
+def factorial(n):
+    result = 5 * 4 *3 * 2 * 1 
+    return result
+
+print(factorial(5))
